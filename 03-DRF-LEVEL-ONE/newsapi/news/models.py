95caf422f19f2a45f88d9bc22f8440a714abb7f6
pymike00 <pymike00@gmail.com>
2019-02-21 16:55:14 +0100

    s03les02 - newsapi project setup

---
+++ 03-DRF-LEVEL-ONE/newsapi/news/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Article(models.Model):
+    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=120)
+    description = models.CharField(max_length=200)
+    body = models.TextField()
+    location = models.CharField(max_length=120)
+    publication_date = models.DateField()
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{ self.author } { self.title }"
