852faf6fedc176a11ffbae03b2ac3aad9faef60d
pymike00 <pymike00@gmail.com>
2019-02-25 19:56:27 +0100

    s03lec08 - the ModelSerializer Class

--- 03-DRF-LEVEL-ONE/newsapi/news/api/serializers.py
+++ 03-DRF-LEVEL-ONE/newsapi/news/api/serializers.py
@@ -1,35 +1,24 @@
+from datetime import datetime
+from django.utils.timesince import timesince
 from rest_framework import serializers
 from news.models import Article
 
 
-class ArticleSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    author = serializers.CharField()
-    title = serializers.CharField()
-    description = serializers.CharField()
-    body = serializers.CharField()
-    location = serializers.CharField()
-    publication_date = serializers.DateField()
-    active = serializers.BooleanField()
-    created_at = serializers.DateTimeField(read_only=True)
-    updated_at = serializers.DateTimeField(read_only=True)
+class ArticleSerializer(serializers.ModelSerializer):
 
-    def create(self, validated_data):
-        print(validated_data)
-        return Article.objects.create(**validated_data)
+    time_since_publication = serializers.SerializerMethodField()
 
-    def update(self, instance, validated_data):
-        instance.author = validated_data.get('author', instance.author)
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', 
-                                                  instance.description)
-        instance.body = validated_data.get('body', instance.body)
-        instance.location = validated_data.get('location', instance.location)
-        instance.publication_date = validated_data.get('publication_data', 
-                                                       instance.publication_date)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
+    class Meta:
+        model = Article
+        exclude = ("id",)
+        # fields = "__all__" # we want all the fields of our model
+        # fields = ("title", "description", "body") # we want to choose a couple of fields!
+
+    def get_time_since_publication(self, object):
+        publication_date = object.publication_date
+        now = datetime.now()
+        time_delta = timesince(publication_date, now)
+        return time_delta
 
     def validate(self, data):
         """ check that description and title are different 
@@ -40,9 +29,56 @@ class ArticleSerializer(serializers.Serializer):
         return data
 
     def validate_title(self, value):
-        """ check that title is at least 60 chars long
+        """ check that title is at least 30 chars long
         https://www.django-rest-framework.org/api-guide/serializers/#field-level-validation
         """
-        if len(value) < 60:
-            raise serializers.ValidationError("The title has to be at least 60 chars long!")
+        if len(value) < 30:
+            raise serializers.ValidationError("The title has to be at least 30 chars long!")
         return value
+
+
+
+# class ArticleSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     author = serializers.CharField()
+#     title = serializers.CharField()
+#     description = serializers.CharField()
+#     body = serializers.CharField()
+#     location = serializers.CharField()
+#     publication_date = serializers.DateField()
+#     active = serializers.BooleanField()
+#     created_at = serializers.DateTimeField(read_only=True)
+#     updated_at = serializers.DateTimeField(read_only=True)
+
+#     def create(self, validated_data):
+#         print(validated_data)
+#         return Article.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         instance.author = validated_data.get('author', instance.author)
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.description = validated_data.get('description', 
+#                                                   instance.description)
+#         instance.body = validated_data.get('body', instance.body)
+#         instance.location = validated_data.get('location', instance.location)
+#         instance.publication_date = validated_data.get('publication_data', 
+#                                                        instance.publication_date)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+#         return instance
+
+#     def validate(self, data):
+#         """ check that description and title are different 
+#         https://www.django-rest-framework.org/api-guide/serializers/#object-level-validation
+#         """
+#         if data["title"] == data["description"]:
+#             raise serializers.ValidationError("Title and Description must be different from one another!")
+#         return data
+
+#     def validate_title(self, value):
+#         """ check that title is at least 60 chars long
+#         https://www.django-rest-framework.org/api-guide/serializers/#field-level-validation
+#         """
+#         if len(value) < 60:
+#             raise serializers.ValidationError("The title has to be at least 60 chars long!")
+#         return value
