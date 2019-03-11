85a5ffc84192b929d0cc2b97ce9777bca9bd363f
pymike00 <pymike00@gmail.com>
2019-03-11 18:52:54 +0100

    s05lec08 - The Filtering System

--- 05-DRF-LEVEL-THREE/profilesapi/profiles/api/urls.py
+++ 05-DRF-LEVEL-THREE/profilesapi/profiles/api/urls.py
@@ -5,7 +5,7 @@ from profiles.api.views import (AvatarUpdateView, ProfileViewSet,
 
 router = DefaultRouter()
 router.register(r"profiles", ProfileViewSet)
-router.register(r"status", ProfileStatusViewSet)
+router.register(r"status", ProfileStatusViewSet, basename="status")
 
 urlpatterns = [
     path("", include(router.urls)),
--- 05-DRF-LEVEL-THREE/profilesapi/profiles/api/views.py
+++ 05-DRF-LEVEL-THREE/profilesapi/profiles/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, mixins, viewsets
+from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
@@ -25,12 +26,21 @@ class ProfileViewSet(mixins.UpdateModelMixin,
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated, IsOwnProfileOrReadOnly]
+    filter_backends = [SearchFilter]
+    search_fields = ["city"]
 
 
 class ProfileStatusViewSet(ModelViewSet):
-    queryset = ProfileStatus.objects.all()
     serializer_class = ProfileStatusSerializer
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        queryset = ProfileStatus.objects.all()
+        username = self.request.query_params.get("username", None)
+        if username is not None:
+            queryset = queryset.filter(user_profile__user__username=username)
+        return queryset
+
 
     def perform_create(self, serializer):
         user_profile = self.request.user.profile
