25fa40a7dbcd786e682560dd50dc1504c8c77af4
pymike00 <pymike00@gmail.com>
2019-03-12 12:55:15 +0100

    s05lec09 - Automated Testing - Part One

--- 05-DRF-LEVEL-THREE/profilesapi/profiles/signals.py
+++ 05-DRF-LEVEL-THREE/profilesapi/profiles/signals.py
@@ -5,6 +5,6 @@ from profiles.models import Profile
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
-    print("Created: ", created)
+#     print("Created: ", created)
     if created:
         Profile.objects.create(user=instance)
--- 05-DRF-LEVEL-THREE/profilesapi/profiles/tests.py
+++ 05-DRF-LEVEL-THREE/profilesapi/profiles/tests.py
@@ -1,3 +1,62 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+
+from profiles.api.serializers import ProfileSerializer
+from profiles.models import Profile
+
+
+class RegistrationTestCase(APITestCase):
+
+    def test_registration(self):
+        data = {"username": "testcase", "email": "test@localhost.app",
+                "password1": "some_strong_psw", "password2": "some_strong_psw"}
+        response = self.client.post("/api/rest-auth/registration/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class ProfileViewSetTestCase(APITestCase):
+
+    list_url = reverse("profile-list")
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="davinci",
+                                             password="some-very-strong-psw")
+        self.token = Token.objects.create(user=self.user)
+        self.api_authentication()
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
+
+    def test_profile_list_authenticated(self):
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_profile_list_un_authenticated(self):
+        self.client.force_authenticate(user=None)
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_profile_detail_retrieve(self):
+        response = self.client.get(reverse("profile-detail", kwargs={"pk": 1}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["user"], "davinci")
+
+    def test_profile_update_by_owner(self):
+        response = self.client.put(reverse("profile-detail", kwargs={"pk": 1}),
+                                   {"city": "Anchiano", "bio": "Renaissance Genius"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content),
+                         {"id": 1, "user": "davinci", "bio": "Renaissance Genius",
+                          "city": "Anchiano", "avatar": None})
+
+    def test_profile_update_by_random_user(self):
+        random_user = User.objects.create_user(username="random", password="psw123123123")
+        self.client.force_authenticate(user=random_user)
+        response = self.client.put(reverse("profile-detail", kwargs={"pk": 1}),
+                                   {"bio": "hacked!!!"})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
