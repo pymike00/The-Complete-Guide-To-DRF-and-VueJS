321da221d032ba53151d3fb978d8df2d1d19f326
pymike00 <pymike00@gmail.com>
2019-03-12 14:07:05 +0100

    s05lec10 - Automated Testing - Part Two

--- 05-DRF-LEVEL-THREE/profilesapi/profiles/tests.py
+++ 05-DRF-LEVEL-THREE/profilesapi/profiles/tests.py
@@ -6,8 +6,8 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 
-from profiles.api.serializers import ProfileSerializer
-from profiles.models import Profile
+from profiles.api.serializers import ProfileSerializer, ProfileStatusSerializer
+from profiles.models import Profile, ProfileStatus
 
 
 class RegistrationTestCase(APITestCase):
@@ -55,8 +55,64 @@ class ProfileViewSetTestCase(APITestCase):
                           "city": "Anchiano", "avatar": None})
 
     def test_profile_update_by_random_user(self):
-        random_user = User.objects.create_user(username="random", password="psw123123123")
+        random_user = User.objects.create_user(username="random", 
+                                               password="psw123123123")
         self.client.force_authenticate(user=random_user)
         response = self.client.put(reverse("profile-detail", kwargs={"pk": 1}),
                                    {"bio": "hacked!!!"})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class ProfileStatusViewSetTestCase(APITestCase):
+
+    url = reverse("status-list")
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="davinci",
+                                             password="some-very-strong-psw")
+        self.status = ProfileStatus.objects.create(user_profile=self.user.profile,
+                                                   status_content="status test")
+        self.token = Token.objects.create(user=self.user)
+        self.api_authentication()
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
+
+    def test_status_list_authenticated(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_status_list_un_authenticated(self):
+        self.client.force_authenticate(user=None)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_status_create(self):
+        data = {"status_content": "a new status!"}
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["user_profile"], "davinci")
+        self.assertEqual(response.data["status_content"], "a new status!")
+
+    def test_single_status_retrieve(self):
+        serializer_data = ProfileStatusSerializer(instance=self.status).data
+        response = self.client.get(reverse("status-detail", kwargs={"pk": 1}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_data = json.loads(response.content)
+        self.assertEqual(serializer_data, response_data)
+
+    def test_status_update_owner(self):
+        data = {"status_content": "content updated"}
+        response = self.client.put(reverse("status-detail", kwargs={"pk": 1}),
+                                   data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["status_content"], "content updated")
+
+    def test_status_update_random_user(self):
+        random_user = User.objects.create_user(username="random", 
+                                               password="psw123123123")
+        self.client.force_authenticate(user=random_user)
+        data = {"status_content": "You Have Been Hacked!"}
+        response = self.client.put(reverse("status-detail", kwargs={"pk": 1}),
+                                   data=data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
