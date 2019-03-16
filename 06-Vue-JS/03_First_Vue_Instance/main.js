0a1d23dd209e57209eb64af7f50dd1e358e7c825
pymike00 <pymike00@gmail.com>
2019-03-16 16:33:15 +0100

    s06lec03 - First Vue Instance

---
+++ 06-Vue-JS/03_First_Vue_Instance/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: "Hello World!",
+        value: 5,
+        imgSrc: "https://cdn.pixabay.com/photo/2011/12/14/12/23/solar-system-11111_960_720.jpg",
+        link: "https://vuejs.org"
+    }
+})
