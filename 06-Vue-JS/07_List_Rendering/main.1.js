4c5fb1eca327f55bf8f48482a8aeb9edba79400f
pymike00 <pymike00@gmail.com>
2019-03-18 12:42:13 +0100

    s06lec07 - List Rendering

---
+++ 06-Vue-JS/07_List_Rendering/main.1.js
@@ -0,0 +1,6 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        users: ["alice", "bob", "batman", "robin", "superman"]
+    }
+}) 
---
+++ 06-Vue-JS/07_List_Rendering/main.js
@@ -0,0 +1,31 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        users: [{
+            id: 567,
+            name: "alice",
+            profession: "developer"
+        },
+        {
+            id: 568,
+            name: "bob",
+            profession: "developer"
+        },
+        {
+            id: 569,
+            name: "batman",
+            profession: "manager"
+        },
+        {
+            id: 570,
+            name: "robin",
+            profession: "designer"
+        }, 
+        {
+            id: 571,
+            name: "superman",
+            profession: "developer"
+        }]
+    }
+}) 
+
