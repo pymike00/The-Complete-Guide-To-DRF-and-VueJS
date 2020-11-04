dc51bc547b2b8fa998e8a0d527d415b87f0d6a3c
Michele Saba <pymike00@gmail.com>
2020-11-04 11:46:47 +0100

    onlinestore.products.views code formatting (section 2)

--- 02-WEB-APIs/First_API_Django/onlinestore/products/views.py
+++ 02-WEB-APIs/First_API_Django/onlinestore/products/views.py
@@ -1,34 +1,37 @@
 from django.http import JsonResponse
-from .models import Product, Manufacturer
+
+from .models import Manufacturer, Product
+
 
 def product_list(request):
-    products = Product.objects.all() # [:30]
-    data = {"products": list(products.values())} # .values("pk", "name")
+    products = Product.objects.all()  # [:30]
+    data = {"products": list(products.values())}  # .values("pk", "name")
     response = JsonResponse(data)
     return response
+
 
 def product_detail(request, pk):
     try:
         product = Product.objects.get(pk=pk)
     except Product.DoesNotExist:
-        response = JsonResponse({
-            "error": {
-                "code": 404,
-                "message": "product not found!"
-            }},
-            status=404)
+        response = JsonResponse(
+            {"error": {"code": 404, "message": "product not found!"}}, status=404
+        )
     else:
-        data = {"product": {
-                    "name": product.name,
-                    "manufacturer": product.manufacturer.name,
-                    "description": product.description,
-                    "photo": product.photo.url,
-                    "price": product.price,
-                    "shipping_cost": product.shipping_cost,
-                    "quantity": product.quantity,                   
-                }}
+        data = {
+            "product": {
+                "name": product.name,
+                "manufacturer": product.manufacturer.name,
+                "description": product.description,
+                "photo": product.photo.url,
+                "price": product.price,
+                "shipping_cost": product.shipping_cost,
+                "quantity": product.quantity,
+            }
+        }
         response = JsonResponse(data)
     return response
+
 
 def manufacturer_list(request):
     manufacturers = Manufacturer.objects.filter(active=True)
@@ -36,24 +39,24 @@ def manufacturer_list(request):
     response = JsonResponse(data)
     return response
 
+
 def manufacturer_detail(request, pk):
     try:
         manufacturer = Manufacturer.objects.get(pk=pk)
         manufacturer_products = manufacturer.products.all()
-        data = {"manufacturer": {
-                    "name": manufacturer.name,
-                    "location": manufacturer.location,
-                    "active": manufacturer.active,
-                    "products": list(manufacturer_products.values())    
-                }}
+        data = {
+            "manufacturer": {
+                "name": manufacturer.name,
+                "location": manufacturer.location,
+                "active": manufacturer.active,
+                "products": list(manufacturer_products.values()),
+            }
+        }
         response = JsonResponse(data)
     except Manufacturer.DoesNotExist:
-        response = JsonResponse({
-            "error": {
-                "code": 404,
-                "message": "manufacturer not found!"
-            }},
-            status=404)
+        response = JsonResponse(
+            {"error": {"code": 404, "message": "manufacturer not found!"}}, status=404
+        )
     return response
 
 
