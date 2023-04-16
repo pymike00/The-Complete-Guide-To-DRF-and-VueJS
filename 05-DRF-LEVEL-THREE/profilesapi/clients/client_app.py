fd5879f5a5b5d8a5ebee6b3ea10dd92f523fa7c8
pymike00 <pymike00@gmail.com>
2023-04-16 21:59:38 +0200

    REFACTOR: client_app

--- 05-DRF-LEVEL-THREE/profilesapi/clients/client_app.py
+++ 05-DRF-LEVEL-THREE/profilesapi/clients/client_app.py
@@ -2,18 +2,18 @@ import requests
 
 
 def registration(account_data: dict) -> None:
-    registration_endpoint = "http://127.0.0.1:8000/api/dj-rest-auth/registration/"
+    registration_endpoint = "http://127.0.0.1:8000/auth/users/"
     response = requests.post(registration_endpoint, data=account_data)
     print(response)
 
 
 def login(login_credentials: dict) -> str:
-    login_endpoint = "http://127.0.0.1:8000/api/dj-rest-auth/login/"
+    login_endpoint = "http://127.0.0.1:8000/auth/token/login/"
     response = requests.post(login_endpoint, data=login_credentials)
     status_code = response.status_code
     print(f"Status Code: {response.status_code}")
     if status_code == 200:
-        auth_token = response.json()["key"]
+        auth_token = response.json()["auth_token"]
         print(f"Auth Token: {auth_token}")
         return auth_token
 
@@ -31,14 +31,14 @@ def client(auth_token: str, endpoint: str) -> None:
 
 if __name__ == "__main__":
     # account_data = {
-    #     "username": "mytestuser",
-    #     "email": "test@myrest.com",
-    #     "password1": "verystrongpassword",
-    #     "password2": "verystrongpassword",
+    #     "username": "mytestuserx",
+    #     "email": "test@myrest.comx",
+    #     "password": "verystrongpassword",
+    #     "re_password": "verystrongpassword",
     # }
     # registration(account_data)
 
-    login_credentials = {"username": "mytestuser", "password": "verystrongpassword"}
+    login_credentials = {"username": "mytestuserx", "password": "verystrongpassword"}
     auth_token = login(login_credentials)
 
     data_endpoint = "http://127.0.0.1:8000/api/profiles/"
